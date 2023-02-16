80bad1621467889e1794ad5a22f7825764d1ede0
Shubh Almal <shubhalmal@gmail.com>
2023-02-16 11:36:31 +0530

    m

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from Data_auditing import *
+import random
+import numpy as np
 
 df = pd.read_csv('Fifa_world_cup_matches.csv')
-# print(df)
 
 print(remove_duplicate_rows(df))
 
-import random
 
 # df['NEW'] = pd.Series(
 #     random.choices(['yes', 'no'], weights=[1, 1], k=len(df)), 
@@ -13,9 +13,6 @@ import random
 # )
 
 # print(replace_string_int(df))
-# print(df)
-
-import numpy as np
 
 
 min_value = 0
@@ -25,6 +22,5 @@ df['col2'] = np.random.randint(min_value, max_value, size=len(df))
 
 # print(replace_string_int(df))
 
-print(df)
 
 print(check_range_dataset(df))
