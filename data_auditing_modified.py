9acb27164489d5e744f630beabe8fb7252f0ab43
Shubh Almal <shubhalmal@gmail.com>
2023-03-22 16:13:16 +0530

    Changes

--- data_auditing_modified.py
+++ data_auditing_modified.py
@@ -3,27 +3,11 @@ import numpy as np
 import random 
 import json
 
-#A function which calls all the other functions
-## Function uniqe_value_col prints lots of data so it has been put as a comment 
-def all(dataset):
-    print(make_changes(dataset))
-    print(data_types(dataset))
-    print(num_col_rows(dataset))
-    print(get_features(dataset))
-    print(statistical_values(dataset))
-    print(frequency_of_word(dataset))
-    # print(uniqe_value_col(dataset))
-    print(remove_duplicate_rows(dataset))
-    print(count_of_each_value(dataset))
-    print(percent_notnull(dataset))
-    print(percent_missing_values(dataset))
-    print(finding_small_dataset(dataset))
-    print(replace_string_int(dataset))
-    print(check_range_dataset(dataset))
-    print(number_within_range(dataset))
 
 
-#This function adds extra columns to check whether other functions are working or not
+
+#making changes in the dataset to make sure all the functions are working
+#Use this for checking. Not entirely needed
 def make_changes(dataset):
         #create a new column for checking
     dataset['NEW'] = pd.Series(
@@ -36,11 +20,63 @@ def make_changes(dataset):
     dataset['col2'] = np.random.randint(min_value, max_value, size=len(dataset))
 
 
+#Drops the columns which are not needed
+#LIST OF COLUMN NAMES TO BE DROPPED
+def drop_columns(dataset, column_names):
+    dataset = dataset.drop(columns=column_names)
+    # Print the resulting DataFrame
+    # print(dataset)
+    return dataset
 
-# Finding datatype of each column
-def data_types(dataset,output_file_name):
+#Finding datatype of the list of columns provided
+#LIST OF COLUMNS
+def data_types_per_col1(dataset,output_file_name,column_names):
     with open(output_file_name, 'a') as file:
-        file.write("Data Types are:\n") 
+        file.write("DATA TYPES ARE:\n") 
+    for column in column_names:
+        ctype = str(dataset[column].dtype)
+        with open(output_file_name, 'a') as file:
+            file.write("column name "+column+ " is of type : "+ctype)
+            file.write("\n")
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
+#Finding Datatype for all the column in a dataset
+# FOR ALL COLUMNS
+def data_types_per_col(dataset,output_file_name):
+    with open(output_file_name, 'a') as file:
+        file.write("DATA TYPES ARE:\n") 
+    for column in dataset.columns:
+        ctype = str(dataset[column].dtype)
+        with open(output_file_name, 'a') as file:
+            file.write("column name "+column+ " is of type : "+ctype)
+            file.write("\n")
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
+# Finding COUNT of each kind of datatype peresnt in the list of columns
+#LIST OF COLUMNS
+def count_of_data_types1(dataset,output_file_name, column_names):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nCOUNT OF DATA TYPES ARE: \n") 
+    column_data_type = {}
+    for column in column_names:
+        ctype = str(dataset[column].dtype)
+        if ctype in column_data_type:
+            column_data_type[ctype] += 1
+        else:
+            column_data_type[ctype] = 1
+    with open(output_file_name, 'a') as file:
+        file.write(json.dumps(column_data_type))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
+
+# Finding COUNT of each kind of datatype peresnt in the whole CSV file
+# FOR ALL COLUMNS
+def count_of_data_types(dataset,output_file_name):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nCOUNT OF DATA TYPES ARE: \n") 
     column_data_type = {}
     for column in dataset.columns:
         ctype = str(dataset[column].dtype)
@@ -48,44 +84,77 @@ def data_types(dataset,output_file_name):
             column_data_type[ctype] += 1
         else:
             column_data_type[ctype] = 1
-    with open(output_file_name, 'a') as convert_file:
-        convert_file.write(json.dumps(column_data_type))
+    with open(output_file_name, 'a') as file:
+        file.write(json.dumps(column_data_type))
+        file.write("\n**************\n")
 
-    # return type(column_data_type)
 
 
 # Finding number of rows and Number of Colunms
+#This Runs ONLY for the WHOLE DATASET
 def num_col_rows(dataset,output_file_name):
     with open(output_file_name, 'a') as file:
-        file.write("\n\nThe Number of Rows and Columns are \n")
+        file.write("\n\nTHE NUMBER OF ROWS AND COLUMNS ARE: \n")
     rows = dataset.shape[0]
     columns = dataset.shape[1]
     result = ("Number of rows " + str(rows) + " and the number of columns are " + str(columns))
     with open(output_file_name, 'a') as file:
         file.write(result)
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
 
 
 #Finding List of features of each Column
+#Runs only for ONLY WHOLE DATASET 
 def get_features(dataset,output_file_name):
     with open(output_file_name, 'a') as file:
-        file.write("\n\nThe list of features are\n")
+        file.write("\n\nTHE LIST OF FEATURES ARE\n")
     features = dataset.columns.tolist()
-    with open(output_file_name, 'a') as convert_file:
-        convert_file.write(json.dumps(features))
-
-
-
-
-# Removing Duplicates
-# def remove_duplicates(dataset):
-#     dataset = dataset.drop_duplicates()
-#     return dataset
-
+    with open(output_file_name, 'a') as file:
+        file.write(json.dumps(features))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
 
 
 # STATISTICAL VALUE
+# Finding Statistical value for list of Column
+# LIST OF COLUMNS
+def statistical_values1(dataset,output_file_name, column_names):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nSTATISTICAL VALUES ARE :")
+    for column in column_names:   
+        if(dataset[column].dtype == np.int64 or  dataset[column].dtype == np.int32):    
+            #Mean
+            mean = dataset[column].mean()
+            #Median
+            median = dataset[column].median()
+            #Standard Deviation 
+            std = dataset[column].std()
+            #Minimum value of column
+            minimum = dataset[column].min()
+            #Maximum Value of Column
+            maximum = dataset[column].max()
+            #Quartile of column
+            quartiles = dataset[column].quantile([0.25, 0.5, 0.75])
+        else:
+            continue        
+        with open(output_file_name, 'a') as file:
+            file.write("\nColumn name = " + column)
+            file.write("\nMean = "+ str(mean))
+            file.write("\nMedian = "+ str(median))
+            file.write("\nStandard Deviation = "+ str(std))
+            file.write("\nMinimum value is = "+ str(minimum))
+            file.write("\nMaximum value is = "+ str(maximum))
+            file.write("\nQuartile = "+ str(quartiles))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
 # Finding Statistical value of each Column
+# FOR ALL COLUMNS
 def statistical_values(dataset,output_file_name):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nSTATISTICAL VALUES ARE :")
     for column in dataset.columns:   
         if(dataset[column].dtype == np.int64 or  dataset[column].dtype == np.int32):    
             #Mean
@@ -100,143 +169,214 @@ def statistical_values(dataset,output_file_name):
             maximum = dataset[column].max()
             #Quartile of column
             quartiles = dataset[column].quantile([0.25, 0.5, 0.75])
-
         else:
             continue        
         with open(output_file_name, 'a') as file:
-            file.write("\n\nColumn name = " + column)
+            file.write("\nColumn name = " + column)
             file.write("\nMean = "+ str(mean))
             file.write("\nMedian = "+ str(median))
             file.write("\nStandard Deviation = "+ str(std))
             file.write("\nMinimum value is = "+ str(minimum))
             file.write("\nMaximum value is = "+ str(maximum))
             file.write("\nQuartile = "+ str(quartiles))
-
-            
-
-#This function is only for a particular column
-def frequency_of_word(dataset,output_file_name):
-    word_counts = dataset["Gender"].str.split().explode().value_counts()
-    maxx = word_counts.max()
-    minn = word_counts.min()
-    # print("Word with Maximum occurance ="+word_counts.idxmax())
-    # print("Word with Minimum occurance ="+word_counts.idxmin())
     with open(output_file_name, 'a') as file:
-        file.write("\n\nMaximum Frequency of column")
-        file.write("Max: "+ str(maxx)+" Min: "+str(minn))
-    
+        file.write("\n**************\n")
 
-#unique values in columns
-#######did not append in the file
-def uniqe_value_col(dataset):
+#USE THIS FUNCTION CAREFULLY AS IT MIGHT RESULT IN LARGE NUMBER OR OUTPUTS
+#This Function counts the frequecy of every unique value
+#LIST OF COLUMNS
+def uniqe_value_col1(dataset,output_file_name,colummn_names):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nUNIQUE VALUES ARE\n")
+    for column in colummn_names:
+        unique_values, frequencies = np.unique(dataset[column], return_counts=True)
+        with open(output_file_name, 'a') as file:
+            file.write(f"Column Name: {column}")
+            file.write("\n")
+            for value, frequency in zip(unique_values, frequencies):
+                file.write(f"Value: {value}, Frequency: {frequency}")
+                file.write("     ")
+            file.write("\n")
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
+#USE THIS FUNCTION CAREFULLY AS IT MIGHT RESULT IN LARGE NUMBER OR OUTPUTS
+#This Function counts the frequecy of every unique value
+#FOR ALL COLUMNS
+def uniqe_value_col(dataset,output_file_name):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nUNIQUE VALUES ARE\n")
     for column in dataset.columns:
         unique_values, frequencies = np.unique(dataset[column], return_counts=True)
+        with open(output_file_name, 'a') as file:
+            file.write(f"Column Name: {column}")
+            file.write("\n")
+            for value, frequency in zip(unique_values, frequencies):
+                file.write(f"Value: {value}, Frequency: {frequency}")
+                file.write("     ")
+            file.write("\n")
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
 
-        print(f"Column: {column}")
-        for value, frequency in zip(unique_values, frequencies):
-            print(f"Value: {value}, Frequency: {frequency}")
-        print("\n")
-# print(uniqe_value_col(df))
 
-
-# Here to count the number of duplicate rows, we do not run the make_changes() Function
+# Here to count the number of duplicate rows and drop them, we do not run the make_changes() Function
+#Runs for WHOLE DATASET
 def remove_duplicate_rows(dataset,output_file_name):
     duplicate_rows = dataset[dataset.duplicated(keep=False)]
     dataset = dataset.drop_duplicates()
-    print(dataset)
-    # return len(duplicate_rows)
+    # print(dataset)
     with open(output_file_name, 'a') as file:
         file.write("\n\nNumber of Duplicate rows:"+ str(len(duplicate_rows))) 
-
-
-
-####add the value which has minimum
-def count_of_each_value1(dataset, coname,output_file_name):
-    # for a single column
-    value_counts = dataset[coname].value_counts()
-    min_frequency = value_counts.min()
-    max_frequency = value_counts.max()
-    # print("Word with Maximum occurance ="+str(value_counts.idxmax()))
-    # print("Word with Minimum occurance ="+str(word_counts.idxmin()))
     with open(output_file_name, 'a') as file:
-        file.write("\n\nCount of each value")
-        file.write("\nColumn Name : "+ coname) 
-        file.write("\nMax count: "+ str(max_frequency)+" Min count: "+str(min_frequency))
+        file.write("\n**************\n")
+    
 
 
-#count of max and min value in each column
-def count_of_each_value(dataset):
-    ## for a single column
-    # value_counts = dataset['Handcap'].value_counts()
-    # min_frequency = value_counts.min()
-    # max_frequency = value_counts.max()
-    # print(value_counts)
-    for column in dataset.columns:
+
+#Display the word with the count which occurs maximum and minimum times
+#LIST OF COLUMNS 
+def count_of_each_value1(dataset,output_file_name,column_names):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nCOUNT OF EACH VALLUE IS: ")
+    for column in column_names:
         value_counts = dataset[column].value_counts()
-        print("Column Name is :" + column)
         min_frequency = value_counts.min()
         max_frequency = value_counts.max()
-        print(value_counts)
-        print(min_frequency)
-        print(max_frequency)
-        print("\n\n")
+        # print("Word with Maximum occurance ="+str(value_counts.idxmax()))
+        # print("Word with Minimum occurance ="+str(value_counts.idxmin()))
+        with open(output_file_name, 'a') as file:
+            file.write("\n\nColumn Name : "+ column) 
+            file.write("\nWord with Maximum occurance = "+str(value_counts.idxmax()))
+            file.write("\nMax count: "+ str(max_frequency))
+            file.write("\nWord with Minimum occurance = "+str(value_counts.idxmin()))
+            file.write("\nMin count: "+str(min_frequency))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
+#Display the word with the count which occurs maximum and minimum times
+#ALL COLUMNS
+def count_of_each_value(dataset,output_file_name):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nCOUNT OF EACH VALLUE IS: ")
+    for column in dataset.columns:
+        value_counts = dataset[column].value_counts()
+        min_frequency = value_counts.min()
+        max_frequency = value_counts.max()
+        # print("Word with Maximum occurance ="+str(value_counts.idxmax()))
+        # print("Word with Minimum occurance ="+str(value_counts.idxmin()))
+        with open(output_file_name, 'a') as file:
+            file.write("\n\nColumn Name : "+ column) 
+            file.write("\nWord with Maximum occurance = "+str(value_counts.idxmax()))
+            file.write("\nMax count: "+ str(max_frequency))
+            file.write("\nWord with Minimum occurance = "+str(value_counts.idxmin()))
+            file.write("\nMin count: "+str(min_frequency))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
 
 
-def percent_notnull1(dataset, colname):
-    val = dataset[colname].count()
-    percent = ((val/len(dataset))*100)
-    return("Notnull: " + str(percent))  
-#percent of Not Null Values
+#Find % of not Null values
+# LIST OF COLUMNS
+def percent_notnull1(dataset,output_file_name,column_names):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nPERCENTAGE NOT NULL")
+    for column in column_names:
+        val = dataset[column].count()
+        with open(output_file_name, 'a') as file:
+            file.write("\n\nName of column = " + column)
+            file.write("\n")
+            file.write(str((val/len(dataset))*100))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
+#Find % of not Null values
+# ALL THE COLUMN
 def percent_notnull(dataset,output_file_name):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nPERCENTAGE NOT NULL")
     for column in dataset.columns:
         val = dataset[column].count()
         with open(output_file_name, 'a') as file:
             file.write("\n\nName of column = " + column)
             file.write("\n")
             file.write(str((val/len(dataset))*100))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
 
-def percent_missing1(dataset, colname):
-    val = dataset[colname].isnull().sum()
-    percent = ((val/len(dataset))*100)
-    return("Missing: " + str(percent))  
-#Percentage of missing data
+
+#Find % of Missing Values
+#LIST OF COLUMNS
+def percent_missing_values1(dataset,output_file_name,column_names):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nPERCENTAGE MISSING VALUES : ")
+    for column in column_names:
+        val = dataset[column].isnull().sum()
+        with open(output_file_name, 'a') as file:
+            file.write("\n\nName of column = " + column)
+            file.write("\n")
+            file.write(str((val/len(dataset))*100))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
+
+#Find % of Missing Values
+#ALL COLUMNS
 def percent_missing_values(dataset,output_file_name):
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nPERCENTAGE MISSING VALUES : ")
     for column in dataset.columns:
         val = dataset[column].isnull().sum()
         with open(output_file_name, 'a') as file:
             file.write("\n\nName of column = " + column)
             file.write("\n")
             file.write(str((val/len(dataset))*100))
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
 
-#Finding a small dataset
+
+#ONLY FOR WHOLE DATASET
 def finding_small_dataset(dataset,output_file_name):
     res = dataset.head()
     with open(output_file_name, 'a') as file:
         file.write("\n\nSample Data : ")
         print(res, file=file)
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
         
 
+
+
+# THE BELOW FUNCTIONS ARE FOR SPECIFIC COLUMNS AND REQUIRE SPECIFIC CHANGES.
+# PLEASE MAKE THE CHANGES ACCORDINGLY
+# INSTRUCTIONS ARE GIVEN ACCORDINGLY ABOVE EACH FUNCTION
+
 #Cannot add to txt file because it updates csv
-def replace_string_int(dataset):
+#add the necessary STRING to INT values as a dictinary shown below
+def replace_string_int(dataset, col_name):
     #save the dictionary with the values which are to be replaced
     str_to_int = {"No": 0, "Yes" :1 }
     #select the column to replace values
-    dataset['NEW'] = dataset['NEW'].map(str_to_int)
+    dataset[col_name] = dataset[col_name].map(str_to_int)
+
 
 #Cannot add to txt file because it updates csv
-def check_range_dataset(dataset):
-    low = 10
-    upper = 110
-    dataset = dataset[(dataset['col2'] >= low) & (dataset['col2'] <= upper)]
+#to make changes to the dataset by filtering the given range provided
+#please change the range according to your needs
+def check_range_dataset(dataset,col_name,l_range,u_range):
+    low = l_range
+    upper = u_range
+    dataset = dataset[(dataset[col_name] >= low) & (dataset[col_name] <= upper)]
     return dataset
 
 
 #to check number of columns within range and percentage
-def number_within_range(dataset,output_file_name):
-    low = 10
-    upper = 110
-    count = len(dataset[(dataset['col2'] >= low) & (dataset['col2'] <= upper)])
+def number_within_range(dataset,output_file_name, col_name,l_range,u_range):
     with open(output_file_name, 'a') as file:
-        file.write("\n\nNumber of rows within the range = " + str(count) )
+        file.write("\n\nNUMBER OF VALUES WITHIN THE GIVEN RANGE ARE : ")
+    low = l_range
+    upper = u_range
+    count = len(dataset[(dataset[col_name] >= low) & (dataset[col_name] <= upper)])
+    with open(output_file_name, 'a') as file:
+        file.write("\n\nColumn Name :" +col_name)
+        file.write("\nNumber of rows within the range = " + str(count) )
         file.write("\nPercent within range = " +str((count/dataset.shape[0])*100))
- 
+    with open(output_file_name, 'a') as file:
+        file.write("\n**************\n")
--- main_modified.py
+++ main_modified.py
@@ -7,21 +7,22 @@ import os
 
 # df = pd.read_csv('KaggleV2-May-2016.csv')
 
-#This adds time to the text file every time we need to find store the output
+column_names = ['PatientId','AppointmentID','Gender']
+columns_to_be_dropped = ['PatientId']
+
 def add_time(output_file_name):
     time_now = time.ctime()
     with open(output_file_name, 'a') as file:
         file.write(time_now)
         file.write("\n\n")
 
-#Adds spacing in text file for better readability 
 def add_spacing(output_file_name):
-    with open(output_file_name, 'a') as file:
-        file.write("\n\n")    
+    with open(output_file_name, 'a') as file:    
         file.write("**************")
+        file.write("\n**************")
+        file.write("\n**************")
         file.write("\n\n")
 
-#compares the new dataframe to the original dataframe
 def compare_df(df1,df2):
     if df1.equals(df2):
         print("The files are identical.")
@@ -29,19 +30,14 @@ def compare_df(df1,df2):
         print("The files are different.")
 
 
-#Add the list of CSV files to be run without the '.csv' extension
 csv_output_file_names = ['KaggleV2-May-2016']
 
-#loop to run all the csv file
 for i in csv_output_file_names:
     fname = i+'.csv'
-    #read csv file
     df = pd.read_csv(fname)
     # print(df)
     print("**************")
     output_file_name = i+'.txt'
-
-    #to check whether the text file for a particular csv file is present or not
     try:
         with open(output_file_name) as file:
             print("File exists")
@@ -53,32 +49,84 @@ for i in csv_output_file_names:
     # to add extra columns to check Data auditing functions
     make_changes(df)
 
-    #add time
+
+    ## final list of functions 
     add_time(output_file_name)
-    #Auditing Functions
-    data_types(df,output_file_name)
-    data_types(df,output_file_name)
+    
+    data_types_per_col1(df, output_file_name, column_names)
+    # data_types_per_col(df, output_file_name, column_names)
+
+    count_of_data_types1(df, output_file_name, column_names)
+    # count_of_data_types(df,output_file_name)
+
     num_col_rows(df,output_file_name)
+
     get_features(df,output_file_name)
-    statistical_values(df,output_file_name)
-    frequency_of_word(df,output_file_name)
+
+    statistical_values1(df, output_file_name, column_names)
+    # statistical_values(df, output_file_name)
+
+
+    #USE THIS FUNCTION CAREFULLY AS IT MIGHT RESULT IN LARGE NUMBER OR OUTPUTS
+    # uniqe_value_col1(df,output_file_name,column_names)
+    # uniqe_value_col(df,output_file_name)
+
     remove_duplicate_rows(df,output_file_name)
-    count_of_each_value1(df,"Gender",output_file_name)
-    percent_missing_values(df,output_file_name)
+
+
+    count_of_each_value1(df, output_file_name, column_names)
+    # count_of_each_value(df,output_file_name)
+
+    percent_notnull1(df,output_file_name, column_names)
+    # percent_notnull(df,output_file_name)
+
+    percent_missing_values1(df,output_file_name, column_names)
+    # percent_missing_values(df,output_file_name)
+
     finding_small_dataset(df,output_file_name)
-    number_within_range(df,output_file_name)
-    # add spacing
+
+    #please add column name according your needs. 
+    #also create multiple function calls for multiple columns
+    replace_string_int(df,"NEW")
+
+    #please add column name according your needs. 
+    #also create multiple function calls for multiple columns
+    #please enter the range according to your needs
+    df = check_range_dataset(df,'col2',10,50)
+
+
+    #please add column name according your needs. 
+    #also create multiple function calls for multiple columns
+    #please enter the range according to your needs
+    number_within_range(df,output_file_name,'col2',10,50)
+
+
     add_spacing(output_file_name)
 
-    # create a updated copy of the csv file after making changes
     edited_csv = i + '_copy.csv'
-    replace_string_int(df)
-    check_range_dataset(df)
+    df = check_range_dataset(df,"col2",10,50)
+    df = drop_columns(df, columns_to_be_dropped)
+
     df.to_csv(edited_csv)
     
 
 
+#     #Auditing functions 
+#     add_time(output_file_name)
+    # data_types(df,output_file_name)
+#     data_types(df,output_file_name)
+#     num_col_rows(df,output_file_name)
+#     get_features(df,output_file_name)
+#     statistical_values(df,output_file_name)
+#     frequency_of_word(df,output_file_name)
+#     remove_duplicate_rows(df,output_file_name)
+#     count_of_each_value1(df,"Gender",output_file_name)
+#     percent_missing_values(df,output_file_name)
+#     finding_small_dataset(df,output_file_name)
+#     number_within_range(df,output_file_name)
+#     add_spacing(output_file_name)
 
+#################################################################3
 
 
 # methods_list = ['data_types','num_col_rows','get_features','statistical_values', 'frequency_of_word', 'remove_duplicate_rows','count_of_each_value','percent_notnull','percent_missing_values','finding_small_dataset','replace_string_int','check_range_dataset','number_within_range']
@@ -92,11 +140,6 @@ for i in csv_output_file_names:
 # # print(csv_coppy())
 # copy_csv = output_file_names[1]+"copy.csv"
 # df.to_csv("fil.csv")
-
-
-
-# print("**********************")
-# compare_df(pd.read_csv('file.csv'),pd.read_csv('KaggleV2-May-2016.csv'))
 
 
 
@@ -114,14 +157,6 @@ for i in csv_output_file_names:
 
 
 
-
-# # Read the CSV files into DataFrames
-# df1 = pd.read_csv("KaggleV2-May-2016.csv")
-# df2 = pd.read_csv("file.csv")
-# diff_df = df1.compare(df2)
-
-# # Print the differences
-# print(diff_df)
 
 
 
